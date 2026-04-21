fc3d011ff5f13e4df93c78a72556c935c8f36783
Rishav_Giri <The0bello0fg0gaze>
2026-04-21 11:09:39 +0530

    images updated

--- frontend/src/app/pages/Root.tsx
+++ frontend/src/app/pages/Root.tsx
@@ -1,6 +1,8 @@
 import { Outlet, Link, useLocation } from "react-router";
 import { HardHat, Menu, X, Facebook, Twitter, Linkedin, Instagram } from "lucide-react";
 import { useState, useEffect } from "react";
+import logo from "/src/app/data/logo.jpeg";
+import logodark from "/src/app/data/logodark.png";
 
 export default function Root() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -22,13 +24,18 @@ export default function Root() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-20">
             {/* Logo */}
-            <Link to="/" className="flex items-center gap-3 group">
-              <div className="bg-amber-500 text-white p-2.5 rounded-lg group-hover:bg-amber-600 transition-colors">
-                <HardHat className="w-6 h-6" />
-              </div>
-              <span className="font-bold text-2xl tracking-tight text-neutral-900"> Excavation Brothers</span>
-            </Link>
-
+		<Link to="/" className="flex items-center gap-3 group">
+		  <div className="p-1 rounded-lg">
+		    <img 
+		      src={logo}
+		      alt="Excavation Brothers Logo" 
+		      className="w-20 h-20 object-contain" 
+		    />
+		  </div>
+		  <span className="font-bold text-2xl tracking-tight text-neutral-900">
+		    Excavation Brothers
+		  </span>
+		</Link>
             {/* Desktop Navigation */}
             <nav className="hidden md:flex items-center space-x-8">
               {navLinks.map((link) => (
@@ -97,9 +104,13 @@ export default function Root() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="col-span-1 lg:col-span-1">
               <Link to="/" className="flex items-center gap-3 group mb-4">
-                <div className="bg-amber-500 text-white p-2 rounded-md">
-                  <HardHat className="w-5 h-5" />
-                </div>
+		  <div className="p-1 rounded-lg">
+		    <img 
+		      src={logodark}
+		      alt="Excavation Brothers Logo" 
+		      className="w-10 h-10 object-contain" 
+		    />
+		  </div>
                 <span className="font-bold text-xl tracking-tight"> Excavation Brothers</span>
               </Link>
               <p className="text-neutral-400 text-sm leading-relaxed mb-6">
